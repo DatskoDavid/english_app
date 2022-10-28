334fa57c3709625fb37f2d531cab08392b48c634
david <datskodavid@gmail.com>
2022-10-28 17:25:37 +0300

    add validation when add new word

--- lib/presentation/screens/training_mode_screens/input_word_screen.dart
+++ lib/presentation/screens/training_mode_screens/input_word_screen.dart
@@ -1,47 +1,64 @@
 import 'package:flutter/material.dart';
 
+import '../../../data/get_word_service.dart';
+import '../../widgets/next_screen_btn.dart';
 import 'result_screen.dart';
 
 class InputWordScreen extends StatelessWidget {
-  const InputWordScreen({Key? key}) : super(key: key);
+  final Future<WordApi> word;
+
+  const InputWordScreen({super.key, required this.word});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('InputWordScreen'),
+        title: const Text('Enter word screen'),
       ),
       body: Center(
-        child: ListView(
-          padding: const EdgeInsets.all(20),
-          children: [
-            const Text('definition'),
-            const SizedBox(height: 20),
-            const TextField(
-              decoration: InputDecoration(hintText: 'Input word'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) {
-                      return const ResultScreen();
-                    },
+        child: FutureBuilder<WordApi>(
+          future: word,
+          builder: (context, snapshot) {
+            if (snapshot.data == null) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            return ListView(
+              padding: const EdgeInsets.all(20),
+              children: [
+                Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 100, 190, 103),
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(
+                      snapshot.data!.results[0].definition,
+                      style: Theme.of(context).textTheme.headline6,
+                      textAlign: TextAlign.center,
+                    ),
                   ),
-                );
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.green),
-              ),
-              child: const Text(
-                'Next',
-                style: TextStyle(
-                  color: Colors.white,
                 ),
-              ),
-            ),
-          ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        filled: true,
+                        // fillColor: Colors.white70,
+                        hintText: 'Enter here',
+                        hintStyle: TextStyle(color: Colors.grey[800])),
+                  ),
+                ),
+                const NextScreenBtn(ResultScreen()),
+              ],
+            );
+          },
         ),
       ),
     );
